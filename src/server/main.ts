0f5ffc552f5d4a1a3a28ab26c1d40f942d5dd670
Neil Kalman <Neilkalman@gmail.com>
2018-11-12 13:55:01 +0200

    feat(services): add a global kibibit-logger as a default logger
    should add back the info function, and allow us to extend the logger in the future

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,8 +1,13 @@
+import { KibibitLoggerService } from './services/kibibit-logger.service';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useLogger(app.get(KibibitLoggerService));
+
   await app.listen(3000);
 }
+
 bootstrap();

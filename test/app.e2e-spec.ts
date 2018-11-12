ea644b1cf9d6aba70be7a036d47eb5ef58e51229
Neil Kalman <Neilkalman@gmail.com>
2018-11-12 02:12:36 +0200

    ci(build): test linter again now with codacy

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,6 +17,7 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
+  
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())

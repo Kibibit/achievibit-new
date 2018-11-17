b9e081e7feb76e3d558d8a0651b49635bbb37bde
Neil Kalman <Neilkalman@gmail.com>
2018-11-17 03:41:55 +0200

    should lint this time

--- src/server/app.service.ts
+++ src/server/app.service.ts
@@ -3,9 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   root(): string {
+    const arr = ['hello', 'world',];
     return 'Hello World!';
   }
 }
 
 // change another file!
 // test4
+// no new line. hope this will be mad!
